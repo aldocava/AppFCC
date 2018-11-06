846b7ac94acee23653c37e6d89de67a9e6b1e89a
aldocava_ <aldo.castvar@gmail.com>
2018-11-06 00:53:16 -0600

    no se

--- application/controllers/Ingreso.php
+++ application/controllers/Ingreso.php
@@ -5,18 +5,23 @@ class Ingreso extends CI_Controller {
 
     public function __construct(){
         parent::__construct();
+        $this->load->model('ingreso_model');
     }
 
 	public function index(){
+        $algo = array('nose' => $this->ingreso_model->obtener_usuario() );
         $this->load->view('common/header');
-		$this->load->view('common/navbar');
+		$this->load->view('common/navbar' );
         //$this->load->view('ingreso/sesion');
-		$this->load->view('common/footer');
+		$this->load->view('common/footer', $algo);
 
 	}
 
     public function verificar(){
-        // code...
+        $nueva_pswd = password_hash($this->input->post('pswd'));
+
+        $nose = $this->ingreso_model();
+        var_dump($nose);
     }
 
     public function salir(){
--- application/models/ingreso_model.php
+++ application/models/ingreso_model.php
@@ -5,5 +5,24 @@ class ingreso_model extends CI_Model {
     public function __construct(){
         parent::__construct();
     }
-    
+
+
+    public function obtener_usuario(){
+        $this->db->select('*');
+        $this->db->from('login');
+        return $this->db->get()->result_array();
+    }
+
+    public function verificar($user, $pswd){
+        $this->db->select('*');
+        $this->db->from('login');
+        $this->db->where('Usuario', $user);
+        $this->db->where('Contraseña', $pswd);
+        $query = $this->db->get()->result_array();
+        if ($query != null) {
+            return $query;
+        }else {
+            return false;
+        }
+    }
 }
